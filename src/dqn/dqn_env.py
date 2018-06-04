0bd7f8b135d099d396b895fe3e1330429258c85f
axmand <815966508@qq.com>
2018-06-04 21:44:53 +0800

    fix

--- src/dqn/dqn_env.py
+++ src/dqn/dqn_env.py
@@ -1,11 +1,14 @@
 """
 Reinforcement learning maze example.
+
 Red rectangle:          explorer.
 Black rectangles:       hells       [reward = -1].
 Yellow bin circle:      paradise    [reward = +1].
 All other states:       ground      [reward = 0].
+
 This script is the environment part of this example.
 The RL is in RL_brain.py.
+
 View more on my tutorial page: https://morvanzhou.github.io/tutorials/
 """
 import numpy as np
@@ -124,3 +127,4 @@ class Maze(tk.Tk, object):
     def render(self):
         # time.sleep(0.01)
         self.update()
+
